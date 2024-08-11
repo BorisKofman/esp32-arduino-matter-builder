8828e6e8ca4e80f886eaf40940733d202668128c
BorisKofman <Borisk@salt.security>
2024-08-11 13:22:17 +0300

    Update download.sh

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 cd tools
 
-ESP_IDF_VERSION="v4.4.6"
-MATTER_VERSION="c49852a"
-ESP_MATTER_VERSION="dd4f34e"
-ARDUINO_ESP32_VERSION="2.0.14"
+ESP_IDF_VERSION="4.4.8"
+MATTER_VERSION="181b0cb"
+ESP_MATTER_VERSION="48751df"
+ARDUINO_ESP32_VERSION="2.0.17"
 
-git clone --recursive https://github.com/espressif/esp-idf
+GIT_SSL_NO_VERIFY=true git clone --recursive https://github.com/espressif/esp-idf
 git -C esp-idf checkout $ESP_IDF_VERSION
 git -C esp-idf submodule update --init --recursive
 
-git clone --recursive https://github.com/espressif/esp-matter
+GIT_SSL_NO_VERIFY=true git clone --recursive https://github.com/espressif/esp-matter
 git -C esp-matter checkout $ESP_MATTER_VERSION
 git -C esp-matter submodule update --init --recursive
 
 git -C esp-matter/connectedhomeip/connectedhomeip checkout $MATTER_VERSION
 
-git clone https://github.com/espressif/arduino-esp32
+GIT_SSL_NO_VERIFY=true git clone https://github.com/espressif/arduino-esp32
 git -C arduino-esp32 checkout $ARDUINO_ESP32_VERSION
 git -C arduino-esp32 submodule update --init --recursive
